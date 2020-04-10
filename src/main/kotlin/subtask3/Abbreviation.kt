6d473792be5f8b5dd04dded204edc8802e15cd7c
KITKATJN <lvutinilyha@gmail.com>
2020-04-10 15:03:07 +0300

    add: Abbreviation

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,26 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val inputStringA=a.toUpperCase()
+        val inputStringB=b.toMutableList()
+        val aListChar=inputStringA.toMutableList()
+        val intersect=aListChar.intersect(inputStringB).toMutableList()
+        return if(intersect==inputStringB){
+            "YES"
+        }else "NO"
+
+       // var bInA:Int=0
+      //  for(i in inputStringB.indices) {
+       //     for (j in inputStringA.indices) {
+       //         bInA++
+       //         if(inputStringB[i]==inputStringA[j]){
+                  //  bInA++
+       //         }
+//
+     //       }
+
+    //    }
+    //    if (bInA<b.length)return "NO $bInA"
+     //   else return "YES $bInA"
     }
 }
