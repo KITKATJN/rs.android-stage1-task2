12f67fb91193d446653e747ba6c6f6f147e325a5
KITKATJN <lvutinilyha@gmail.com>
2020-04-12 14:41:27 +0300

    add: fibonacci

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,18 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var fibonacci= arrayListOf(0,1)
+        while (fibonacci[0]*fibonacci[1]<n){
+            fibonacci[1]=(fibonacci[1]+fibonacci[0]).also { fibonacci[0]=fibonacci[1] }
+        }
+
+      if (fibonacci[0]*fibonacci[1]==n) {
+            fibonacci.add(1)
+            return fibonacci.toIntArray()
+        }else {
+          fibonacci.add(0)
+          return fibonacci.toIntArray()
+      }
     }
 }
+
