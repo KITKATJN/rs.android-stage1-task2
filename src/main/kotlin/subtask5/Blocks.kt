091e1a88322d6858aaffd779978fa43123e04afd
KITKATJN <lvutinilyha@gmail.com>
2020-04-12 14:11:24 +0300

    add: Blocks

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,21 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+       return when(blockB){
+           Int::class->blockA.filterIsInstance<Int>().sum()
+           String::class->blockA.filterIsInstance<String>().joinToString("")
+           LocalDate::class->{
+               val nearDate: LocalDate = blockA.filterIsInstance<LocalDate>().max() as LocalDate
+               nearDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+           }
+           else->"Hello kotlin"
+       }
     }
 }
